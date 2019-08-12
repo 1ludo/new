7aad4b93ac3cc2d1cc9a97270d85d847f074a37d
1ludo <53859919+1ludo@users.noreply.github.com>
2019-08-12 10:49:07 +0200

    Add files via upload

---
+++ passwordAccesss.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+
+{
+    string const bitcoinPK("5KfXfW4x9NS8nRNd1P4sC9ezJHZnBZjgnGcfBr9yeaVBDf1AkVg");
+    int const password(123);
+    int input;
+    int trials(3);
+
+    cout << "Pour acceder a votre cle prive, saisissez votre mot de passe: " << endl;
+
+    do
+    {
+        cin >> input;
+
+        if (input != password)
+        {
+            trials--;
+        }
+        if (input != password && trials == 1 || input != password && trials == 2)
+        {
+            cout << "Tentatives restantes : " << trials << endl;
+        }
+        if (trials == 0)
+        {
+            cout << "Vous avez depasser le nombre de tentatives autorises." << endl;
+        }
+
+    } while (input != password && trials > 0);
+
+
+    if (input == password)
+    {
+        cout << "Authentification reussie !" << endl;
+        cout << "Voici votre cle prive : " << bitcoinPK << endl;
+    }
+    else
+    {
+        cout << "Echec d'authentification, fin du programme." << endl;
+    }
+
+    return 0;
+}
