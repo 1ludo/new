3de184b4479aaebd87cd23202a5cadd1eb0c825e
1ludo <53859919+1ludo@users.noreply.github.com>
2020-10-01 16:08:26 +0200

    Update passwordAccesss.cpp

--- passwordAccesss.cpp
+++ passwordAccesss.cpp
@@ -4,44 +4,31 @@
 using namespace std;
 
 int main()
-
 {
-    string const bitcoinPK("5KfXfW4x9NS8nRNd1P4sC9ezJHZnBZjgnGcfBr9yeaVBDf1AkVg");
-    int const password(123);
-    int input;
-    int trials(3);
+	string const bitcoinPK("5KfXfW4x9NS8nRNd1P4sC9ezJHZnBZjgnGcfBr9yeaVBDf1AkVg");
+	string const password("azer1234");
+	string input("none");
+	int trials(3);
 
-    cout << "Pour acceder a votre cle prive, saisissez votre mot de passe: " << endl;
+	cout << "To access your private key, please enter your password : " << endl;
 
-    do
-    {
-        cin >> input;
+	do {
+		cin >> input;
+		trials--;
+		if (input != password && trials > 0) {
+			cout << "Password Incorrect." << endl << "Trials left : " << trials << endl;
+		}
+	} while (input != password && trials > 0);
+	
+	if (input == password) {
+		cout << "Successful Authentication !" << endl << "Here is your private key : " << bitcoinPK << endl;
+	}
+	else if (input != password && trials == 0) {
+		cout << "Authentication Failure." << endl << "You have exceeded the number of authorized attempts." << endl;
+	}
+	
+	cout << "End of the program." << endl;
 
-        if (input != password)
-        {
-            trials--;
-        }
-        if (input != password && trials == 1 || input != password && trials == 2)
-        {
-            cout << "Tentatives restantes : " << trials << endl;
-        }
-        if (trials == 0)
-        {
-            cout << "Vous avez depasser le nombre de tentatives autorises." << endl;
-        }
-
-    } while (input != password && trials > 0);
-
-
-    if (input == password)
-    {
-        cout << "Authentification reussie !" << endl;
-        cout << "Voici votre cle prive : " << bitcoinPK << endl;
-    }
-    else
-    {
-        cout << "Echec d'authentification, fin du programme." << endl;
-    }
-
-    return 0;
+	system("PAUSE");
+	return 0;
 }
